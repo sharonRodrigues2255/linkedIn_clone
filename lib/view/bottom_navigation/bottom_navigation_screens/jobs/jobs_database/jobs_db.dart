b59d0a2b53bfad16563b284c71d5f45014fa97b1
Sharon Rodrigues <sr979151@gmail.com>
2023-09-28 19:27:38 +0530

    Jobs database updated

--- lib/view/bottom_navigation/bottom_navigation_screens/jobs/jobs_database/jobs_db.dart
+++ lib/view/bottom_navigation/bottom_navigation_screens/jobs/jobs_database/jobs_db.dart
@@ -1,130 +1,13 @@
 class JobDatabase {
   static List<Map<String, dynamic>> jobPostings = [
     {
-      'jobTitle': 'Software Engineer',
-      'address': '123 Main Street',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Tech Innovations',
-      'location': 'San Francisco, California, USA',
-      'remoteStatus': 'Hybrid',
-      'timePosted': '2 hours ago',
-    },
-    {
-      'jobTitle': 'Data Scientist',
-      'address': '456 Oak Avenue',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Data Insights',
-      'location': 'New York, New York, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '1 day ago',
-    },
-    {
-      'jobTitle': 'UX/UI Designer',
-      'address': '789 Pine Lane',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Design Studios',
-      'location': 'Los Angeles, California, USA',
-      'remoteStatus': 'Online',
-      'timePosted': '3 days ago',
-    },
-    {
-      'jobTitle': 'Flutter Developer',
-      'address': '101 River Road',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Mobile Apps Inc.',
-      'location': 'Austin, Texas, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '4 days ago',
-    },
-    {
-      'jobTitle': 'Android Developer',
-      'address': '202 Forest Avenue',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Android Solutions',
-      'location': 'Seattle, Washington, USA',
-      'remoteStatus': 'Hybrid',
-      'timePosted': '5 days ago',
-    },
-    {
-      'jobTitle': 'iOS Developer',
-      'address': '303 Mountain Street',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'iOS Innovators',
-      'location': 'Chicago, Illinois, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '6 days ago',
-    },
-    {
-      'jobTitle': 'Front-end Developer',
-      'address': '404 Lakeview Drive',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Web World Inc.',
-      'location': 'Miami, Florida, USA',
-      'remoteStatus': 'Online',
-      'timePosted': '1 week ago',
-    },
-    {
-      'jobTitle': 'Back-end Developer',
-      'address': '505 Oakwood Lane',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Server Solutions',
-      'location': 'Denver, Colorado, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '1 week ago',
-    },
-    {
-      'jobTitle': 'Full Stack Developer',
-      'address': '606 Riverfront Road',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Code Creators',
-      'location': 'Portland, Oregon, USA',
-      'remoteStatus': 'Hybrid',
-      'timePosted': '2 weeks ago',
-    },
-    {
-      'jobTitle': 'UI Designer',
-      'address': '707 Pinecrest Avenue',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'UI Wizards',
-      'location': 'San Diego, California, USA',
-      'remoteStatus': 'Online',
-      'timePosted': '2 weeks ago',
-    },
-    {
-      'jobTitle': 'Quality Assurance Engineer',
-      'address': '808 Cypress Street',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'QA Experts',
-      'location': 'Houston, Texas, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '3 weeks ago',
-    },
-    {
-      'jobTitle': 'Product Manager',
-      'address': '909 Elmwood Drive',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'Product Visionaries',
-      'location': 'Phoenix, Arizona, USA',
-      'remoteStatus': 'Hybrid',
-      'timePosted': '3 weeks ago',
-    },
-    {
-      'jobTitle': 'DevOps Engineer',
-      'address': '1010 Oakridge Avenue',
-      'image': 'https://via.placeholder.com/150',
-      'companyName': 'DevOps Solutions',
-      'location': 'Minneapolis, Minnesota, USA',
-      'remoteStatus': 'Remote',
-      'timePosted': '4 weeks ago',
-    },
-    {
       'jobTitle': 'Network Engineer',
       'address': '1111 Maple Drive',
       'image': 'https://via.placeholder.com/150',
       'companyName': 'Network Innovators',
       'location': 'Philadelphia, Pennsylvania, USA',
       'remoteStatus': 'Online',
-      'timePosted': '4 weeks ago',
+      'timePosted': '4 weeks ago'
     },
     {
       'jobTitle': 'Machine Learning Engineer',
@@ -133,7 +16,7 @@ class JobDatabase {
       'companyName': 'AI Creations',
       'location': 'Atlanta, Georgia, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '5 weeks ago',
+      'timePosted': '5 weeks ago'
     },
     {
       'jobTitle': 'Data Analyst',
@@ -142,7 +25,7 @@ class JobDatabase {
       'companyName': 'Data Crunchers',
       'location': 'Boston, Massachusetts, USA',
       'remoteStatus': 'Online',
-      'timePosted': '5 weeks ago',
+      'timePosted': '5 weeks ago'
     },
     {
       'jobTitle': 'Cybersecurity Analyst',
@@ -151,7 +34,7 @@ class JobDatabase {
       'companyName': 'Secure Solutions',
       'location': 'Washington, D.C., USA',
       'remoteStatus': 'Remote',
-      'timePosted': '6 weeks ago',
+      'timePosted': '6 weeks ago'
     },
     {
       'jobTitle': 'Cloud Solutions Architect',
@@ -160,7 +43,7 @@ class JobDatabase {
       'companyName': 'Cloud Innovations',
       'location': 'Dallas, Texas, USA',
       'remoteStatus': 'Hybrid',
-      'timePosted': '7 weeks ago',
+      'timePosted': '7 weeks ago'
     },
     {
       'jobTitle': 'Game Developer',
@@ -169,7 +52,7 @@ class JobDatabase {
       'companyName': 'Game Studios',
       'location': 'Orlando, Florida, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '8 weeks ago',
+      'timePosted': '8 weeks ago'
     },
     {
       'jobTitle': 'Embedded Systems Engineer',
@@ -178,7 +61,7 @@ class JobDatabase {
       'companyName': 'Embedded Tech Solutions',
       'location': 'San Jose, California, USA',
       'remoteStatus': 'Online',
-      'timePosted': '8 weeks ago',
+      'timePosted': '8 weeks ago'
     },
     {
       'jobTitle': 'IT Support Specialist',
@@ -187,7 +70,7 @@ class JobDatabase {
       'companyName': 'IT Support Experts',
       'location': 'Raleigh, North Carolina, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '9 weeks ago',
+      'timePosted': '9 weeks ago'
     },
     {
       'jobTitle': 'Technical Writer',
@@ -196,7 +79,7 @@ class JobDatabase {
       'companyName': 'Docs and Code',
       'location': 'Pittsburgh, Pennsylvania, USA',
       'remoteStatus': 'Hybrid',
-      'timePosted': '10 weeks ago',
+      'timePosted': '10 weeks ago'
     },
     {
       'jobTitle': 'Robotics Engineer',
@@ -205,7 +88,7 @@ class JobDatabase {
       'companyName': 'RoboTech Innovations',
       'location': 'Detroit, Michigan, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '11 weeks ago',
+      'timePosted': '11 weeks ago'
     },
     {
       'jobTitle': 'UI/UX Researcher',
@@ -214,7 +97,7 @@ class JobDatabase {
       'companyName': 'Research Labs',
       'location': 'Cambridge, Massachusetts, USA',
       'remoteStatus': 'Online',
-      'timePosted': '12 weeks ago',
+      'timePosted': '12 weeks ago'
     },
     {
       'jobTitle': 'Systems Administrator',
@@ -223,7 +106,7 @@ class JobDatabase {
       'companyName': 'System Experts',
       'location': 'Salt Lake City, Utah, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '12 weeks ago',
+      'timePosted': '12 weeks ago'
     },
     {
       'jobTitle': 'Business Analyst',
@@ -232,7 +115,7 @@ class JobDatabase {
       'companyName': 'Business Solutions',
       'location': 'Kansas City, Missouri, USA',
       'remoteStatus': 'Hybrid',
-      'timePosted': '13 weeks ago',
+      'timePosted': '13 weeks ago'
     },
     {
       'jobTitle': 'Network Security Engineer',
@@ -241,7 +124,7 @@ class JobDatabase {
       'companyName': 'Security Innovations',
       'location': 'Charlotte, North Carolina, USA',
       'remoteStatus': 'Online',
-      'timePosted': '14 weeks ago',
+      'timePosted': '14 weeks ago'
     },
     {
       'jobTitle': 'Product Designer',
@@ -250,7 +133,7 @@ class JobDatabase {
       'companyName': 'Product Design Studios',
       'location': 'Denver, Colorado, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '15 weeks ago',
+      'timePosted': '15 weeks ago'
     },
     {
       'jobTitle': 'Digital Marketing Specialist',
@@ -259,7 +142,7 @@ class JobDatabase {
       'companyName': 'Digital Marketing Experts',
       'location': 'Austin, Texas, USA',
       'remoteStatus': 'Hybrid',
-      'timePosted': '16 weeks ago',
+      'timePosted': '16 weeks ago'
     },
     {
       'jobTitle': 'Machine Vision Engineer',
@@ -268,7 +151,7 @@ class JobDatabase {
       'companyName': 'Vision Tech Solutions',
       'location': 'San Diego, California, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '17 weeks ago',
+      'timePosted': '17 weeks ago'
     },
     {
       'jobTitle': 'Sales Representative',
@@ -277,7 +160,7 @@ class JobDatabase {
       'companyName': 'Sales Experts Inc.',
       'location': 'Nashville, Tennessee, USA',
       'remoteStatus': 'Online',
-      'timePosted': '18 weeks ago',
+      'timePosted': '18 weeks ago'
     },
     {
       'jobTitle': 'HR Specialist',
@@ -286,7 +169,7 @@ class JobDatabase {
       'companyName': 'HR Solutions',
       'location': 'Atlanta, Georgia, USA',
       'remoteStatus': 'Remote',
-      'timePosted': '19 weeks ago',
+      'timePosted': '19 weeks ago'
     },
     {
       'jobTitle': 'Technical Support Engineer',
@@ -295,7 +178,187 @@ class JobDatabase {
       'companyName': 'Tech Support Experts',
       'location': 'Dallas, Texas, USA',
       'remoteStatus': 'Hybrid',
-      'timePosted': '20 weeks ago',
+      'timePosted': '20 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3131 Oakwood Drive',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Flutter Tech Solutions',
+      'location': 'San Diego, California, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '21 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3232 Cedar Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'App Innovate',
+      'location': 'Seattle, Washington, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '22 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3333 Pinecrest Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Mobile Magic',
+      'location': 'Los Angeles, California, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '23 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3434 Oakridge Avenue',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Flutter Creations',
+      'location': 'Chicago, Illinois, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '24 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3535 Birch Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'App Wizards',
+      'location': 'New York, New York, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '25 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3636 Pine Street',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Mobile Innovations',
+      'location': 'Denver, Colorado, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '26 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3737 Elmwood Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'App Developers Inc.',
+      'location': 'Miami, Florida, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '27 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3838 Oakwood Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Tech Innovate',
+      'location': 'Atlanta, Georgia, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '28 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '3939 Cedar Avenue',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Mobile Wizards',
+      'location': 'Dallas, Texas, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '29 weeks ago'
+    },
+    {
+      'jobTitle': 'Flutter Developer',
+      'address': '4040 Cypress Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Code Crafters',
+      'location': 'Orlando, Florida, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '30 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4141 Pinecrest Street',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Android Creations',
+      'location': 'San Jose, California, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '31 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4242 Oakwood Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Tech Innovations',
+      'location': 'Austin, Texas, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '32 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4343 Cypress Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'App Solutions',
+      'location': 'Boston, Massachusetts, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '33 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4444 Cedar Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Android Wizards',
+      'location': 'Philadelphia, Pennsylvania, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '34 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4545 Elm Street',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Mobile Tech Solutions',
+      'location': 'Raleigh, North Carolina, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '35 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4646 Pine Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'App Innovators',
+      'location': 'Charlotte, North Carolina, USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '36 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4747 Birch Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Android Solutions Inc.',
+      'location': 'Kansas City, Missouri, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '37 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4848 Oakridge Drive',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Tech Builders',
+      'location': 'Nashville, Tennessee, USA',
+      'remoteStatus': 'Remote',
+      'timePosted': '38 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '4949 Cedar Lane',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Android Innovate',
+      'location': 'Washington, D.C., USA',
+      'remoteStatus': 'Hybrid',
+      'timePosted': '39 weeks ago'
+    },
+    {
+      'jobTitle': 'Android Developer',
+      'address': '5050 Oakwood Road',
+      'image': 'https://via.placeholder.com/150',
+      'companyName': 'Mobile Apps Inc.',
+      'location': 'Salt Lake City, Utah, USA',
+      'remoteStatus': 'Online',
+      'timePosted': '40 weeks ago'
     },
   ];
 }
