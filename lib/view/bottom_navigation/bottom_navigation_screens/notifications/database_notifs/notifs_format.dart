b0b6e116a299ccf3f9d56fe6203fe731ed6946d3
Sharon Rodrigues <sr979151@gmail.com>
2023-09-28 13:23:39 +0530

    Notifications basic  UI finished

--- lib/view/bottom_navigation/bottom_navigation_screens/notifications/database_notifs/notifs_format.dart
+++ lib/view/bottom_navigation/bottom_navigation_screens/notifications/database_notifs/notifs_format.dart
@@ -1,5 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone/utils/contants/colors.dart';
+import 'package:linkedin_clone/utils/contants/contant_sizes.dart';
 import 'package:linkedin_clone/utils/contants/myfont.dart';
+import 'package:linkedin_clone/view/profile_page/widgets/curve_rectangle.dart';
+
+RichText checkTypeNotification(Map<String, dynamic> dbItem) {
+  final type = dbItem["type"];
+
+  switch (type) {
+    case "posted":
+      return Notifs.posted(post: dbItem["post"], name: dbItem["name"]);
+    case "congratsnewposition":
+      return Notifs.congratsNewPosition(
+          name: dbItem['name'],
+          position: dbItem['position'],
+          company: dbItem['company']);
+    case "congratsAniversary":
+      return Notifs.congratsAniversary(
+          name: dbItem['name'],
+          years: dbItem['years'],
+          company: dbItem['company']);
+    case "viewedProfile":
+      return Notifs.viewedProfile(name: dbItem['name']);
+    case "opertunities":
+      return Notifs.opertunities(
+          position: dbItem['position'],
+          number: dbItem['number'],
+          place: dbItem['place']);
+    default:
+      return RichText(text: TextSpan(text: "hehe"));
+  }
+}
+
+Widget returnCurveRectangle(Map<String, dynamic> dbItem) {
+  final type = dbItem["type"];
+  if (type == "congratsnewposition" || type == "congratsAniversary") {
+    return Column(
+      children: [
+        height20,
+        CurveRectangle(
+          mediaWidth: 300,
+          title: "Say Congrats",
+          color: kblue,
+        ),
+      ],
+    );
+  } else if (type == "opertunities") {
+    return Column(
+      children: [
+        height20,
+        CurveRectangle(
+          mediaWidth: 220,
+          title: "View jobs",
+          color: kblue,
+        ),
+      ],
+    );
+  } else {
+    return SizedBox();
+  }
+}
 
 class Notifs {
   static posted({required String post, required String name}) {
@@ -7,7 +67,9 @@ class Notifs {
         text: TextSpan(
             text: name,
             style: myfontNormal(weight: FontWeight.bold),
-            children: [TextSpan(text: "posted : $post")]));
+            children: [
+          TextSpan(text: " posted : $post", style: myfontNormal()),
+        ]));
   }
 
   static congratsNewPosition(
@@ -15,19 +77,21 @@ class Notifs {
       required String position,
       required String company}) {
     return RichText(
-        text: TextSpan(text: "Congratulate ", children: [
+        text: TextSpan(text: "Congratulate ", style: myfontNormal(), children: [
       TextSpan(text: name, style: myfontNormal(weight: FontWeight.bold)),
-      TextSpan(text: "on starting a new position as $position at"),
+      TextSpan(
+          text: " on starting a new position as $position at",
+          style: myfontNormal()),
       TextSpan(text: company, style: myfontNormal(weight: FontWeight.bold))
     ]));
   }
 
   static congratsAniversary(
-      {required String name, required int years, required String company}) {
+      {required String name, required String years, required String company}) {
     return RichText(
-        text: TextSpan(text: "Congratulate", children: [
+        text: TextSpan(text: "Congratulate ", style: myfontNormal(), children: [
       TextSpan(text: name, style: myfontNormal(weight: FontWeight.bold)),
-      TextSpan(text: "on $years year at "),
+      TextSpan(text: " on $years year at ", style: myfontNormal()),
       TextSpan(text: company, style: myfontNormal(weight: FontWeight.bold))
     ]));
   }
@@ -37,19 +101,23 @@ class Notifs {
         text: TextSpan(
             text: name,
             style: myfontNormal(weight: FontWeight.bold),
-            children: [TextSpan(text: "viewed your profile. See all views.")]));
+            children: [
+          TextSpan(
+              text: " viewed your profile. See all views.",
+              style: myfontNormal())
+        ]));
   }
 
   static opertunities(
       {required String position,
       required String number,
       required String place}) {
-    RichText(
+    return RichText(
         text: TextSpan(
             text: "$position $number",
             style: myfontNormal(weight: FontWeight.bold),
             children: [
-          TextSpan(text: "opertunities in"),
+          TextSpan(text: " opertunities in ", style: myfontNormal()),
           TextSpan(text: place, style: myfontNormal(weight: FontWeight.bold))
         ]));
   }
--- lib/view/bottom_navigation/bottom_navigation_screens/notifications/tabs/all.dart
+++ lib/view/bottom_navigation/bottom_navigation_screens/notifications/tabs/all.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone/view/bottom_navigation/bottom_navigation_screens/notifications/database_notifs/db.dart';
+import 'package:linkedin_clone/view/bottom_navigation/bottom_navigation_screens/notifications/database_notifs/notifs_format.dart';
 
 class All extends StatelessWidget {
   const All({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return const ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-        ),
-      );
-    });
+    return ListView.builder(
+        itemCount: NotificationsData.notifications.length,
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                      NotificationsData.notifications[index]['image']),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    checkTypeNotification(
+                      NotificationsData.notifications[index],
+                    ),
+                    Container(
+                      child: returnCurveRectangle(
+                          NotificationsData.notifications[index]),
+                    )
+                  ],
+                ),
+                trailing: Column(
+                  children: [Text("4d"), Icon(Icons.more_vert)],
+                ),
+              ));
+        });
   }
 }
