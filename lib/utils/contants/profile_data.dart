bf8e8155229b6a88fee721978fd406d848850e43
Sharon Rodrigues <sr979151@gmail.com>
2023-09-29 12:57:10 +0530

    updated invitations

--- lib/utils/contants/profile_data.dart
+++ lib/utils/contants/profile_data.dart
@@ -95,6 +95,159 @@ class Profile {
     },
   ];
 
+  static List<Map> invitations = [
+    {
+      "name": "Frank",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person1.jpg",
+      "sent": "10 days ago",
+    },
+    {
+      "name": "Grace",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person2.png",
+      "sent": "5 days ago",
+    },
+    {
+      "name": "Hank",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person3.png",
+      "sent": "15 days ago",
+    },
+    {
+      "name": "Ivy",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person4.png",
+      "sent": "20 days ago",
+    },
+    {
+      "name": "Jack",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person5.jpg",
+      "sent": "8 days ago",
+    },
+    {
+      "name": "Kelly",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person1.jpg",
+      "sent": "12 days ago",
+    },
+    {
+      "name": "Leo",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person2.png",
+      "sent": "3 days ago",
+    },
+    {
+      "name": "Mia",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person3.png",
+      "sent": "18 days ago",
+    },
+    {
+      "name": "Nathan",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person4.png",
+      "sent": "7 days ago",
+    },
+    {
+      "name": "Olivia",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person5.jpg",
+      "sent": "25 days ago",
+    },
+    {
+      "name": "Paul",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person1.jpg",
+      "sent": "14 days ago",
+    },
+    {
+      "name": "Quinn",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person2.png",
+      "sent": "9 days ago",
+    },
+    {
+      "name": "Rachel",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person3.png",
+      "sent": "6 days ago",
+    },
+    {
+      "name": "Sam",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person4.png",
+      "sent": "22 days ago",
+    },
+    {
+      "name": "Tom",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person5.jpg",
+      "sent": "11 days ago",
+    },
+    {
+      "name": "Ursula",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person1.jpg",
+      "sent": "17 days ago",
+    },
+    {
+      "name": "Vince",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person2.png",
+      "sent": "4 days ago",
+    },
+    {
+      "name": "Wendy",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person3.png",
+      "sent": "13 days ago",
+    },
+    {
+      "name": "Xander",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person4.png",
+      "sent": "19 days ago",
+    },
+    {
+      "name": "Yvonne",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person5.jpg",
+      "sent": "16 days ago",
+    },
+    {
+      "name": "Zack",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person1.jpg",
+      "sent": "21 days ago",
+    },
+    {
+      "name": "Amy",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person2.png",
+      "sent": "2 days ago",
+    },
+    {
+      "name": "Ben",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person3.png",
+      "sent": "24 days ago",
+    },
+    {
+      "name": "Cathy",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person4.png",
+      "sent": "23 days ago",
+    },
+    {
+      "name": "Dylan",
+      "headline": "developer",
+      "picture": "assets/images/person_avatars/person5.jpg",
+      "sent": "1 day ago",
+    },
+  ];
+
   static List<Map> peopleIKnow = [
     {
       "name": "Person1",
