e2bfa6540f7d6ea21914d13dff81dc1b58fbc3a4
Sharon Rodrigues <sr979151@gmail.com>
2023-09-29 16:17:19 +0530

    Settings item crated

--- lib/view/settings/settings.dart
+++ lib/view/settings/settings.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone/utils/contants/colors.dart';
+import 'package:linkedin_clone/utils/contants/contant_sizes.dart';
+import 'package:linkedin_clone/utils/contants/myfont.dart';
+import 'package:linkedin_clone/utils/contants/profile_data.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -7,6 +11,50 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
+      appBar: AppBar(
+        actions: [
+          CircleAvatar(
+              radius: 12,
+              backgroundColor: kblack,
+              child: Icon(
+                Icons.question_mark_outlined,
+                size: 20,
+                color: kwhite,
+              )),
+          width10
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(22.0),
+        child: SettingsItems(
+          icon: CircleAvatar(
+            backgroundImage: NetworkImage(Profile.profilePicture),
+          ),
+          title: "Settings",
+          size: 32.0,
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsItems extends StatelessWidget {
+  const SettingsItems(
+      {super.key, required this.icon, required this.title, this.size = 20});
+  final Widget icon;
+  final String title;
+  final double size;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        icon,
+        width10,
+        Text(
+          title,
+          style: myfont(size: size),
+        )
+      ],
     );
   }
 }
---
+++ lib/view/settings/widgets/settings_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:linkedin_clone/utils/contants/contant_sizes.dart';
+import 'package:linkedin_clone/utils/contants/myfont.dart';
+
+class SettingsItems extends StatelessWidget {
+  const SettingsItems(
+      {super.key, required this.icon, required this.title, this.size = 20});
+  final Widget icon;
+  final String title;
+  final double size;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        icon,
+        width10,
+        Text(
+          title,
+          style: myfont(size: size),
+        )
+      ],
+    );
+  }
+}
