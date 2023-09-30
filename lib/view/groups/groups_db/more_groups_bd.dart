94e3abbe5487f4df0751b270928193f0206435ff
Sharon Rodrigues <sr979151@gmail.com>
2023-09-30 12:23:03 +0530

    Groups tabs updateds

--- lib/view/groups/groups_db/more_groups_bd.dart
+++ lib/view/groups/groups_db/more_groups_bd.dart
@@ -1,51 +1,51 @@
 List<Map<String, dynamic>> moreGroups = [
   {
-    "name": "Group 6",
+    "name": "Tech Explorers",
     "members": "98765432",
     "picture": "assets/images/groupsimages/fluttericon.png"
   },
   {
-    "name": "Group 7",
+    "name": "Code Crafters",
     "members": "54321098",
     "picture": "assets/images/groupsimages/flutter.jpg"
   },
   {
-    "name": "Group 8",
+    "name": "Innovative Coders",
     "members": "13579246",
     "picture": "assets/images/groupsimages/fluttericon.png"
   },
   {
-    "name": "Group 9",
+    "name": "Creative Developers",
     "members": "24680135",
     "picture": "assets/images/groupsimages/flutter.jpg"
   },
   {
-    "name": "Group 10",
+    "name": "Digital Mavericks",
     "members": "11223344",
     "picture": "assets/images/groupsimages/fluttericon.png"
   },
   {
-    "name": "Group 11",
+    "name": "Code Pioneers",
     "members": "55667788",
     "picture": "assets/images/groupsimages/flutter.jpg"
   },
   {
-    "name": "Group 12",
+    "name": "Tech Innovators",
     "members": "99001122",
     "picture": "assets/images/groupsimages/fluttericon.png"
   },
   {
-    "name": "Group 13",
+    "name": "Future Tech Leaders",
     "members": "33445566",
     "picture": "assets/images/groupsimages/flutter.jpg"
   },
   {
-    "name": "Group 14",
+    "name": "Coding Visionaries",
     "members": "77889900",
     "picture": "assets/images/groupsimages/fluttericon.png"
   },
   {
-    "name": "Group 15",
+    "name": "Digital Dreamers",
     "members": "12121212",
     "picture": "assets/images/groupsimages/flutter.jpg"
   },
