8828fd2d6538a6a4eed0688d042d67e3501abb33
Sharon Rodrigues <sr979151@gmail.com>
2023-10-04 23:14:16 +0530

    updated posts db with pic and college

--- lib/db/posts_db/posts_db.dart
+++ lib/db/posts_db/posts_db.dart
@@ -5,53 +5,63 @@ List<Map<String, dynamic>> posts = [
     "name": "Alice Johnson",
     "designation": "Data Scientist",
     "timeAgo": "1 hour ago",
+    "picture": "assets/images/person_avatars/person1.jpg",
     "image": PostImages.postimage1,
     "likes": 1020,
     "description":
         "Just finished an exciting data science project. Happy to share my learnings!",
     "isliked": false,
-    "comments": []
+    "comments": [],
+    "colleges": ["College A", "College B"]
   },
   {
     "name": "Bob Anderson",
     "designation": "Product Manager",
     "timeAgo": "3 hours ago",
+    "picture": "assets/images/person_avatars/person2.png",
     "image": PostImages.postImage2,
     "likes": 32440,
     "description": "Launching a new product next week. Stay tuned for updates!",
     "isliked": false,
-    "comments": []
+    "comments": [],
+    "colleges": ["College C", "College D"]
   },
   {
     "name": "Eva White",
     "designation": "Graphic Designer",
     "timeAgo": "6 hours ago",
+    "picture": "assets/images/person_avatars/person3.png",
     "image": PostImages.postImage3,
     "likes": 3453,
     "description": "Designed a stunning new logo. Let me know your thoughts!",
     "isliked": false,
-    "comments": []
+    "comments": [],
+    "colleges": ["College E", "College F"]
   },
   {
     "name": "Charlie Brown",
     "designation": "Marketing Specialist",
     "timeAgo": "10 hours ago",
+    "picture": "assets/images/person_avatars/person4.png",
     "image": PostImages.postImage4,
     "likes": 3453,
     "description":
         "Running a marketing campaign for our latest product. Exciting times ahead!",
     "isliked": false,
-    "comments": []
+    "comments": [],
+    "colleges": ["College G", "College H"]
   },
   {
     "name": "Grace Lee",
     "designation": "iOS Developer",
     "timeAgo": "12 hours ago",
+    "picture": "assets/images/person_avatars/person5.jpg",
     "image": PostImages.postImage5,
     "likes": 97970,
     "description":
         "Just released a major update for our mobile app. Check it out!",
     "isliked": false,
-    "comments": []
+    "comments": [],
+    "colleges": ["College I", "College J"]
   },
 ];
